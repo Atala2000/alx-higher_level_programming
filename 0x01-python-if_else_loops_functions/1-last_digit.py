f823cafefb7bfb1c79beee2d6a07cbf2e338b57d
Atala2000 <atalakidi@gmail.com>
2023-05-09 12:56:29 -0700

    1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 modulus = abs(number) % 10
+if number < 0:
+    modulus = number * -1
+    modulus = modulus % 10
+    modulus =modulus * -1
+else:
+    modulus = number % 10
 if modulus > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, modulus))
 elif modulus == 0:
