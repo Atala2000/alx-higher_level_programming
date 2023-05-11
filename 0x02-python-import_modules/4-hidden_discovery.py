e16b18ebb25f0dc402150ac1a4f3c1e66ec85fad
Atala2000 <atalakidi@gmail.com>
2023-05-11 05:35:27 -0700

    1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 import hidden_4
 
 if __name__ == "__main__":
-file = dir(hidden_4)
-length = len(fl)
-for n in range(0, length):
-    if file[n][0:2] != "__":
-        print(file[x])
+    file = dir(hidden_4)
+    length = len(fl)
+    for n in range(0, length):
+        if file[n][0:2] != "__":
+            print(file[x])
