5b11b46ff8f27c65c5a329ddd5b05fa18570115c
Atala2000 <atalakidi@gmail.com>
2023-05-11 05:29:10 -0700

    1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main__"
+if __name__ == "__main__":
 x = len(sys.argv)
 sum = 0
 for i in range(1, x):
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 fl = dir(hidden_4)
 ll = len(fl)
 for x in range(0, ll):
