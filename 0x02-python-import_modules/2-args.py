f8921dceba76c354ffab5756e53fc1a68e131e1b
Atala2000 <atalakidi@gmail.com>
2023-05-11 05:27:07 -0700

    1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     from sys import argv
 argc = len(argv)
 if argc < 2:
@@ -9,5 +9,5 @@ else:
         print("{} arguments:".format(argc - 1))
     else:
         print("{} arguments:".format(argc - 1))
-    for x in range(1, argc):
+    for n in range(1, argc):
         print("{}: {}".format(n, argv[n]))
