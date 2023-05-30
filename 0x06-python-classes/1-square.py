5660f3bcae45e5ac41e20c95434dd4794f46db81
Atala2000 <atalakidi@gmail.com>
2023-05-30 08:23:33 -0700

    class-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+
+class Square:
+    """Reps a square
+    Attributes:
+    _size (int): square size
+    """
+    def __init__(self, size):
+        """Initialize square
+        Args:
+            size (int): square size
+        Returns: None
+        """
+        self.__size = size
