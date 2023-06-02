958df2396a7c87466fb5a73235ba277749acab7c
Atala2000 <atalakidi@gmail.com>
2023-06-02 04:16:13 -0700

    negative

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     sum1 = []
     for x in argv:
-        if x.isdigit():
+        if x.lstrip("-").isdigit():
             sum1.append(int(x))
     print(sum(sum1))
