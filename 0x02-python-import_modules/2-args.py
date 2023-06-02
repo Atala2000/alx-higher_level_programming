f49b604b9e6a376dd1c9b0343554ddb903a65793
Atala2000 <atalakidi@gmail.com>
2023-06-02 02:58:31 -0700

    2 test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
 pl = (len(argv))
-print(f"{(pl - 1):d} arguments")
-for x in range(1, pl):
-    print(f"{x:d} : {argv[x]}")
+if pl == 2:
+    print(f"{(pl - 1):d} argument:")
+    print(f"{(pl - 1):d}: {argv[1]}")
+elif pl == 1:
+    print(f"{(pl - 1):d} arguments.")
+else:
+    print(f"{(pl - 1):d} arguments:")
+    for x in range(1, pl):
+        print(f"{x:d} : {argv[x]}")
