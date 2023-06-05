764fdb175229db81d6d5e920e80de1a6d010f461
Atala2000 <atalakidi@gmail.com>
2023-06-05 12:36:54 -0700

    9 changed whitespace

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
-    
+
     @classmethod
     def square(cls, size=0):
         """New rectangle"""
