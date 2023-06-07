f92923f4a3f9b7717357bb5655133751afa54365
Atala2000 <111738703+Atala2000@users.noreply.github.com>
2023-06-07 19:30:49 +0300

    Update 1-safe_print_integer.py
    Alternate code implementation

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-
     try:
-        print('{:d}'.format(value))
-    except Exception as error:
-        return False
-    else:
-        return True
+        if type(value) is int:
+            print("{}".format(value))
+            return True
+        else:
+            return False
+    except Exception as e:
+        return e
