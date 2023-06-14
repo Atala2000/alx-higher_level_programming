1586a7139de8ae3d6e5c8a9e3cde9afd7101d404
Atala2000 <atalakidi@gmail.com>
2023-06-14 14:27:10 -0700

    corr to 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ _is_kind_of_class funct
 
 def inherits_from(obj, a_class):
     """True if obj is inst"""
-    return (issubclass(type(obj, a_class) and type(obj) != a_class)
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
