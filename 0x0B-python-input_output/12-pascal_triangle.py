07b142365e8a4d1c1dcef60d09ecaa55c7db04fb
Atala2000 <atalakidi@gmail.com>
2023-06-14 16:17:11 -0700

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,7 +2,7 @@
 """pascatl t"""
 
 
-def pascal_triangle(n:4500):
+def pascal_triangle(n=4500):
     """pascal print"""
     pascal = [[0]*i for i in range(1, n+1)]
     ct = 0
@@ -10,6 +10,6 @@ def pascal_triangle(n:4500):
         pascal[i][0] = 1
         pascal[i][-1] = 1
         for y in range(0, i//2):
-            pasca[i][y+1] = pascal[i-1][y] + pascal[i-1][y+1]
+            pascal[i][y+1] = pascal[i-1][y] + pascal[i-1][y+1]
             pascal[i][i-y-1] = pascal[i-1][y] + pascal[i-1][y+1]
-        return pascal
+    return pascal
