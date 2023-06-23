d1de6f832b01e1125d40b49523d1522904b5c933
Atala2000 <atalakidi@gmail.com>
2023-06-23 06:10:00 -0700

    3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,26 @@ Rectangle class that inherits from Base class
 """
 
 
+class Base:
+    """This is the base model
+    Attributes:
+        __nb_objects (int): number of objects
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """New base
+
+        Args:
+            id (int): Base identity
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
 class Rectangle(Base):
     """
         REctangle class that inherits from BAse
