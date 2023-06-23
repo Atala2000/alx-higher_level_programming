cc7d9fdcbb7bbd9bad70dcf925a814fb73bab65b
Atala2000 <atalakidi@gmail.com>
2023-06-23 05:59:53 -0700

    2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Rectangle class that inherits from Base class
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+        REctangle class that inherits from BAse
+        Methods:
+            __init__()
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+            INIT A CLASS INSTANCE
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
