a8aa469afd338fd0e515b11554b247a14c8f2d8a
Atala2000 <atalakidi@gmail.com>
2023-11-15 21:54:03 +0300

    tasks

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 
-let size = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2]);
+
 if (isNaN(size) || process.argv[2] === undefined) {
   console.log('Missing size');
-}
-let psr = 'X';
-for (let i = 0; i < size; i++) {
-  psr += 'X';
-}
-while (size > 0) {
-  console.log(psr);
-  size--;
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
 }
