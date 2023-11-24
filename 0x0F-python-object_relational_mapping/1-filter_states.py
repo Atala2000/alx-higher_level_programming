69a136d824c5cfef8c8aaca2b8c4e4452cc843f7
Atala2000 <atalakidi@gmail.com>
2023-11-24 09:04:23 +0300

    1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from database
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Allows access to the database
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM hbtn_0e_0_usa.states WHERE name LIKE 'N%'")
+    x = cur.fetchall()
+
+    for results in x:
+        print(f"{results}")
